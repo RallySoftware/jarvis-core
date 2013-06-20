6f6a1f5b17c6ea01e1f3d095bf1cf4a97419ff51
Chauncey <pairingmachine@rallydev.com>
2013-06-20 10:48:01 -0600

    fixing description of ping

--- src/main/clojure/jarvis/plugins/ping.clj
+++ src/main/clojure/jarvis/plugins/ping.clj
@@ -1,6 +1,6 @@
 (ns jarvis.plugins.flows)
 
-(defn ^{:description "ping - I will respond if I am available."
+(defn ^{:description "I will respond if I am available."
         :command "ping"
         :author "Jonathan Chauncey"
         :plugin true}
