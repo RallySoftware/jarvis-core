ed6e53f3ee42dd2ec318588d40fe3e25f1cb061a
Jonathan Chauncey <jchauncey@rallydev.com>
2014-03-26 21:58:38 -0600

    upgrading ot clj-flowdock 1.2.1, version bump to 1.6.2

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.rallydev/jarvis-core "1.6.1"
+(defproject com.rallydev/jarvis-core "1.6.2xs"
   :description "A robot for FlowDock"
   :url "http://github.com/RallySoftware/jarvis-core"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [com.rallydev/clj-flowdock "1.2.0"]
+                 [com.rallydev/clj-flowdock "1.2.1"]
                  [org.codehaus.groovy/groovy-all "2.1.0"]
                  [org.clojure/java.classpath "0.2.0"]
                  [org.clojure/tools.logging "0.2.6"]
